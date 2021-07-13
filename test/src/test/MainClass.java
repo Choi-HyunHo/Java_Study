2a09b277ff6c46c53558ddcc306db1c8eeaa4706
최현호 <x7788@naver.com>
2021-07-13 23:48:14 +0900

    Test

---
+++ test/src/test/MainClass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Java");
+		System.out.println("A");
+	}
+
+}
