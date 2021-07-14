13b914759a0650d888947d4d2f691dde5077e2f8
최현호 <x7788@naver.com>
2021-07-14 14:40:53 +0900

    study

---
+++ Study/src/study/pjtTest_6.java
@@ -0,0 +1,19 @@
+package study;
+
+public class pjtTest_6 {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		
+		System.out.println("++x : " + (++x));
+		System.out.println("--x : " + (--x));
+		
+		System.out.println("x++ : " + (x++));
+		System.out.println("x : " + x);
+		
+		System.out.println("x-- : " + (x--));
+		System.out.println("x : " + x);
+	}
+
+}
