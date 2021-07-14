72af44daf8e8e5924b4c3ea568278a230d850846
최현호 <x7788@naver.com>
2021-07-14 13:54:03 +0900

    study

---
+++ Study/src/study/pjtTest_4.java
@@ -0,0 +1,20 @@
+package study;
+
+public class pjtTest_4 {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		
+		// 대입 연산자
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		
+		x = y;
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+
+	}
+
+}
