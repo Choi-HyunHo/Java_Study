42ebb20df4bb25c930018f4d73fe92b2d947dd9d
최현호 <x7788@naver.com>
2021-07-14 14:25:18 +0900

    study

---
+++ Study/src/study/pjtTest_5.java
@@ -0,0 +1,19 @@
+package study;
+
+public class pjtTest_5 {
+
+	public static void main(String[] args) {
+		
+		int a = 10; 
+		int b = 20;
+		
+		System.out.println("a > b : " + (a > b));
+		System.out.println("a < b : " + (a < b));
+		System.out.println("a >= b : " + (a >= b));
+		System.out.println("a <= b : " + (a <= b));
+		System.out.println("a == b : " + (a == b));
+		System.out.println("a != b : " + (a != b));
+
+	}
+
+}
