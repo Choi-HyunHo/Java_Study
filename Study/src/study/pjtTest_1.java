1c34abe871c1ba1bd5a6bb99d00fb9075e122e6f
최현호 <x7788@naver.com>
2021-07-14 09:41:32 +0900

    변수

---
+++ Study/src/study/pjtTest_1.java
@@ -0,0 +1,16 @@
+package study;
+
+public class pjtTest_1 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		i = 10;
+		System.out.println("i = " + i);
+		
+		int j = 20;
+		System.out.println("j = " + j);
+
+	}
+
+}
