9f1f26a16cf1c87fe6937a36b12af7bc39cf53db
최현호 <x7788@naver.com>
2021-07-14 14:02:55 +0900

    study

--- Study/src/study/pjtTest_4.java
+++ Study/src/study/pjtTest_4.java
@@ -5,15 +5,8 @@ public class pjtTest_4 {
 	public static void main(String[] args) {
 		
 		int x = 10;
-		int y = 20;
-		
-		// 대입 연산자
-		System.out.println("x = " + x);
-		System.out.println("y = " + y);
-		
-		x = y;
-		System.out.println("x = " + x);
-		System.out.println("y = " + y);
+		System.out.println("x += 10 : " + (x += 10));
 
 	}
+
 }
